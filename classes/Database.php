34f181dbad534902a15d89b962f70f4a32e302e1
moortwig <info@moortwig.se>
2015-01-12 22:05:55 +0100

    Cleaned up a little

--- classes/Database.php
+++ classes/Database.php
@@ -6,20 +6,20 @@ require_once("../config/db-config.php");
 class Database() {
 	// some spiffy code here
 	protected $db_conn;
-    public $db_host = $dbHost;
-    public $db_user = $dbUsername;
-    public $db_pass = $dbPassword;
-    public $db_name = $dbName;
+	public $db_host = $dbHost;
+	public $db_user = $dbUsername;
+	public $db_pass = $dbPassword;
+	public $db_name = $dbName;
 
-    function connect() {
-    	try {
-            $this->db_conn = new PDO("mysql:host=$this->db_host;dbname=$this->db_name", $this->db_user,$this->db_pass);
-            return $this->db_conn;
-        }
-        catch(PDOException $e){
-            return $e->getMessage();
-        }
-    }
+	function connect() {
+	// TODO vad innebär try och catch? Varför använder jag det?
+		try {
+			$this->db_conn = new PDO("mysql:host=$this->db_host;dbname=$this->db_name", $this->db_user,$this->db_pass);
+			return $this->db_conn;
+		} catch(PDOException $e) {
+			return $e->getMessage();
+		}
+	}
 }
 
 
