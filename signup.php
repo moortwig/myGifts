c8094605a1a14c61df557a37860aaf82e7e79f62
moortwig <info@moortwig.se>
2015-01-31 12:06:32 +0100

    Code documentation. Cleanup

--- signup.php
+++ signup.php
@@ -10,28 +10,22 @@ if(isset($_POST["signup"])) {
 	// hämta userModel-funktionen
 	$user = new User();
 	// TODO make this prettier
+
 	// get from the form
-	
 	$username = $_POST['username'];
     $password = $_POST['password'];
     $email = $_POST['email'];    
     $joined = $_POST['joined'];
 
-    var_dump($username);
-
-    // THE QUERY insertNewUser() ::::::
-	// $query = $this->prepare("INSERT INTO users (username, password, email, joined) VALUES (?,?,?,?)");
-	// $values = array($username, $password, $email, $joined);
-	// $query->execute($values);
-	// $counts = $query->rowCount();
-	// return $counts;
-
+ 
     // TODO query funkar, men ger nollor i datetime
-	$query = $user->link->prepare("INSERT INTO users (username, password, email, joined) VALUES (?,?,?,?)");
-	$values = array($username, $password, $email, $joined);
-	$query->execute($values);
-	$counts = $query->rowCount();
-	return $counts;
+    // prepare(), execute() and rowCount() are all PDO methods
+	$query = $user->link->prepare('INSERT INTO users (username, password, email, joined) VALUES (?,?,?,?)'); // prepare the query
+	$values = array($username, $password, $email, $joined); // store the input data in an array
+	$query->execute($values); // execute the query with the data, here inserting them into the database
+	// below 2 lines are not necessary to store in database KEEP HERE FOR NOW
+	// $rows = $query->rowCount();
+	// return $rows;
 
 
     // $username = $this->setUsername($username);
