9bd477b77b9b0264acdb87aa298b8a959c667334
moortwig <info@moortwig.se>
2015-03-22 16:03:25 +0100

    Navigation bar shows only if logged in

--- header.php
+++ header.php
@@ -22,10 +22,11 @@ require_once('app.php');
 </head>
 <body>
 
-<nav class="navbar navbar-fixed-top">
-    <div class="container">
-        <?php if(isset($_SESSION['username'])) { ?>
-            <!-- Display this section if a user is logged in -->
+<?php if(isset($_SESSION['username'])) { ?>
+    <!-- Display navigation bar if a user is logged in -->
+    <nav class="navbar navbar-fixed-top">
+        <div class="container">
+
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                     <span class="sr-only">Toggle navigation</span>
@@ -35,18 +36,19 @@ require_once('app.php');
                 </button>
                 <a class="navbar-brand" href="#">myGifts</a>
             </div><!-- .navbar-header -->
-            
+
             <div class="navbar-collapse collapse">
                 <ul class="nav navbar-nav">
-                <li><a href="index.php">Home</a></li>
-                <li><a href="profile.php">Profile</a></li>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="profile.php">Profile</a></li>
                 </ul>
                 <form class="navbar-form navbar-right" method="post" action="app.php" role="form">
                     <input type="submit" class="" name="logout" value="Log out" />
                 </form>
                 <span class="navbar-right navbar-brand">Logged in: <?php echo $_SESSION['username']; ?></span>
-            <?php } else {
-                // Display this section if noone has logged in yet! ?>
+
+            
+                <!-- // Display this section if noone has logged in yet! ?> -->
                 <form class="navbar-form navbar-right" method="post" action="app.php" role="form">
                     <div class="form-group">
                         <input type="text" name="user" placeholder="Username" class="form-control" />
@@ -57,6 +59,6 @@ require_once('app.php');
                     <input type="submit" class="" name="login" value="Log in"  class="btn btn-success"/>
                 </form>
             </div><!-- .navbar-collapse -->
-        <?php } ?>
-    </div><!-- .container -->
-</nav>
+        </div><!-- .container -->
+    </nav>
+<?php }
