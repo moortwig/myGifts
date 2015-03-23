715df2f990e34496e1412466f640b475042f1f43
moortwig <info@moortwig.se>
2015-03-23 20:44:29 +0100

    Added Recipient list and grid

--- addrecipient.php
+++ addrecipient.php
@@ -3,36 +3,66 @@
 require_once('header.php');
 
 ?>
-<div class="main">
+<div class="main container">
     <?php
     // Display this section if a user is logged in
     if(isset($_SESSION['username'])) { ?>
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <form class="form-horizontal" method="post" action="app.php" role="form">
+                    <h2>Add recipient</h2>
+                    <label for="name">Name:</label>
+                    <input type="text" name="name" class="form-control" placeholder="Name" /><br />
+                    <label for="Information">Information:</label><br />
+                    <textarea name="information" class="form-control"></textarea>
+                    <!-- TODO Maybe a better way to pass on the session user ID? -->
+                    <!-- YES! THERE IS!!! Besides, the row below isn't even working ...
+                    Read THIS: http://www.clfsrpm.net/csrf/ -->
+                    <!-- <input name="userId" type="hidden" value=<?php /*echo "'". $_SESSION['userId'] . "'" */?> /> -->
+                    <!-- TODO remove this field with dummy data -->
+                    <input name="userId" type="hidden" value="5" />
 
-        <h2>Add recipient</h2>
-        <div id="addRecipientForm">
-            <!--    TODO adjust this for the new structure!  -->
-            <form class="form-horizontal" method="post" action="app.php" role="form">
-                <label for="name">Name:</label>
-                <input type="text" name="name" class="form-control" placeholder="Name" /><br />
-                <label for="Information">Information:</label><br />
-                <textarea name="information" class="form-control"></textarea>
-                <!-- TODO Maybe a better way to pass on the session user ID? -->
-                <!-- YES! THERE IS!!! Besides, the row below isn't even working ...
-                Read THIS: http://www.clfsrpm.net/csrf/ -->
-                <!-- <input name="userId" type="hidden" value=<?php /*echo "'". $_SESSION['userId'] . "'" */?> /> -->
-                <!-- TODO remove this field with dummy data -->
-                <input name="userId" type="hidden" value="5" />
+                    <br />
+                    <!-- buttons: -->
+                    <!-- TODO jQuery on click clear form
+                    <input type="submit" class="button" name="clear" value="CLEAR" /> -->
 
-                <br />
-                <!-- buttons: -->
-                <!-- TODO jQuery on click clear form
-                <input type="submit" class="button" name="clear" value="CLEAR" /> -->
+                    <!-- TODO JavaScript check that all fields are filled in: -->
+                    <!-- TODO jQuery on key down "Enter" -> Submit -->
+                    <button type="submit" class="btn btn-lg btn-primary btn-block" name="addRecipient">Submit</button>
+                </form>
+            </div><!-- .col-md-4 col-md-offset-4 -->
 
-                <!-- TODO JavaScript check that all fields are filled in: -->
-                <!-- TODO jQuery on key down "Enter" -> Submit -->
-                <input type="submit" class="" name="addRecipient" value="Submit" />
-            </form>
-        </div><!-- #addRecipientForm -->
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <h2>Recipients</h2> 
+                        <?php
+                        $recipient = new Recipient();
+                        $userId = 5; // dummy data TODO change this to the session user
+                        $recipients = $recipient->getAllRecipients($userId);
+                       
+                        foreach ($recipients as $key => $r) {
+                            $recipientId = $r['id'];
+                                            
+                            echo "<div class='data-row'>"; 
+                                // echo "<div class='data-field'>";
+                                    echo "<a href='recipient.php?id=" . $r['id'] . "'>" . $r['name'] . "</a> ";
+                                    // echo $r['name'];
+                                // echo "</div>";
+                                /*echo "<div class='data-field'>";
+                                    echo $r['information'];
+                                echo "</div>";*/
+                                // echo "<div class='data-edit'>";
+                                    echo "<a href='editrecipient.php?id=" . $r['id'] . "'><span class='glyphicon glyphicon-pencil'></span></a> ";
+                                    // echo "<a class='btn btn-default btn-xs' href='editrecipient.php?id=" . $r['id'] . "'>Edit</a> ";
+                                // echo "</div>";
+                            echo "<hr />";
+                            echo "</div>"; ?>      
+                        <?php } ?>
+                    </div><!-- .panel -->
+                </div><!-- .panel -->
+            </div><!-- .col-md-4 -->
     <?php } else { ?>
         <h2>Hold!</h2>
         <p>Access to this content is forbidden. Log in, or sign up and you shall recieve access.</p>
