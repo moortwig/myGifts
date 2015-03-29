b46a0be069e5f7f5382cb9a6cd1f61add664a08f
moortwig <info@moortwig.se>
2015-03-29 22:26:17 +0200

    changed logged in as-text and button order

--- header.php
+++ header.php
@@ -47,9 +47,9 @@ require_once('app.php');
                     <li><a href="addgift.php" class="btn">Add gift</a></li>
                 </ul>
                 <form class="navbar-form navbar-right" method="post" action="app.php" role="form">
+                    <span class="navbar-text">Logged in: <a href="profile.php"><?php echo $_SESSION['username']; ?></a></span>
                     <button type="submit" class="btn btn-danger" name="logout">Log out</button>
                 </form>
-                <p class="navbar-right navbar-text">Logged in: <a href="profile.php"><?php echo $_SESSION['username']; ?></a></p>
             </div><!-- .navbar-collapse -->
         </div><!-- .container -->
     </nav>
