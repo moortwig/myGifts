a7d4944dbf3f99149261130fecc319e3bfad510a
moortwig <info@moortwig.se>
2015-03-31 22:41:08 +0200

    Disabled outlines and added a wrapper to index

--- index.php
+++ index.php
@@ -20,29 +20,30 @@ require_once('header.php');
 
     <?php } else {
 
-        // Display this section if user has not logged in ?>    
-        <div class="row">
-            <form class="form-horizontal" method="post" action="app.php" role="form">
-                <div class="container centered-content block-align">
-                    <div class="col-xs-12 col-md-3 col-md-offset-3 align-columns">
-                        <h2 class="form-signin-heading">Please log in</h2><p class="help-block">to your MyGifts account</p>
-                        <label for="inputUsername" class="sr-only">Username</label>
-                        <input type="text" name="user" placeholder="Username" class="form-control" />
-                        <label for="inputPassword" class="sr-only">Password</label>
-                        <input type="password" name="pass" placeholder="Password" class="form-control" />
-                        <button type="submit" name="login" class="btn btn-lg btn-primary btn-block">LOG IN</button>
-                    </div>
-                    <div class="col-xs-12 col-md-3 align-columns">
-                        <h1>myGifts</h1>
-                        <p>Keep track of items you've given away.
-                        Add your friends and family to your own registry.
-                        Your gift reminder right there in your pocket.
-                        <h4 class="form-signin-heading">Not registered yet?</h4><a href="signup.php" class="btn btn-lg btn-success btn-block">SIGN UP NOW</a>
-                    </div>
-                </div><!-- .container -->
-            </form>
-        </div><!-- .row -->
-
+        // Display this section if user has not logged in ?>
+        <div class="wrapper">
+            <div class="row">
+                <form class="form-horizontal" method="post" action="app.php" role="form">
+                    <div class="container centered-content block-align">
+                        <div class="col-xs-12 col-md-3 col-md-offset-3 align-columns">
+                            <h2 class="form-signin-heading">Please log in</h2><p class="help-block">to your MyGifts account</p>
+                            <label for="inputUsername" class="sr-only">Username</label>
+                            <input type="text" name="user" placeholder="Username" class="form-control" />
+                            <label for="inputPassword" class="sr-only">Password</label>
+                            <input type="password" name="pass" placeholder="Password" class="form-control" />
+                            <button type="submit" name="login" class="btn btn-lg btn-primary btn-block">LOG IN</button>
+                        </div>
+                        <div class="col-xs-12 col-md-3 align-columns">
+                            <h1>myGifts</h1>
+                            <p>Keep track of items you've given away.
+                            Add your friends and family to your own registry.
+                            Your gift reminder right there in your pocket.</p>
+                            <h4 class="form-signin-heading">Not registered yet?</h4><a href="signup.php" class="btn btn-lg btn-success btn-block">SIGN UP NOW</a>
+                        </div>
+                    </div><!-- .container -->
+                </form>
+            </div><!-- .row -->
+        </div><!-- .wrapper -->
         
     <?php } ?>
 
