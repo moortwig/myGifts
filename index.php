5d5a19df31fa64843ea995cf81208ecb1000b3f9
moortwig <info@moortwig.se>
2015-04-07 20:41:50 +0200

    Resetting css because of design changes

--- index.php
+++ index.php
@@ -5,24 +5,23 @@ require_once('header.php');
 ?>
 
 <!-- MAIN BODY -->
-<div class="main">
-   <div class="container">
-        <br />
-        <br />
-        <?php
-        
-        if(isset($_SESSION['username'])) {
-            // Display this section if a user is logged in ?>
-            <p>
-                Ennui Truffaut messenger bag umami, tousled sustainable brunch. Vegan +1 listicle ugh. Blue Bottle shabby chic cray Carles, tote bag fixie +1. Banh mi pug Pinterest lomo, tilde keytar meggings four dollar toast twee locavore plaid literally Vice Truffaut readymade. Whatever Etsy fashion axe asymmetrical cred, shabby chic lumbersexual disrupt kogi cray sartorial artisan YOLO readymade. Pour-over Truffaut you probably haven't heard of them, semiotics gentrify narwhal Godard cronut selvage. Vegan street art +1 hashtag drinking vinegar, wayfarers small batch shabby chic post-ironic swag.
-            </p>
-        </div><!-- .container -->
 
-    <?php } else {
+<div class="index-wrapper">
+    <br />
+    <br />
+    <?php
+    
+    if(isset($_SESSION['username'])) {
+        // Display this section if a user is logged in ?>
+        <p>
+            Ennui Truffaut messenger bag umami, tousled sustainable brunch. Vegan +1 listicle ugh. Blue Bottle shabby chic cray Carles, tote bag fixie +1. Banh mi pug Pinterest lomo, tilde keytar meggings four dollar toast twee locavore plaid literally Vice Truffaut readymade. Whatever Etsy fashion axe asymmetrical cred, shabby chic lumbersexual disrupt kogi cray sartorial artisan YOLO readymade. Pour-over Truffaut you probably haven't heard of them, semiotics gentrify narwhal Godard cronut selvage. Vegan street art +1 hashtag drinking vinegar, wayfarers small batch shabby chic post-ironic swag.
+        </p>
 
-        // Display this section if user has not logged in ?>
-        <div class="wrapper">
-            <div class="row">
+<?php } else {
+    // Display this section if user has not logged in ?>
+    <div class="login-section">
+        <div class="row">
+            <div class="container">
                 <form class="form-horizontal" method="post" action="app.php" role="form">
                     <div class="container centered-content block-align">
                         <div class="col-xs-12 col-md-3 col-md-offset-3 align-columns">
@@ -43,11 +42,11 @@ require_once('header.php');
                     </div><!-- .container -->
                 </form>
             </div><!-- .row -->
-        </div><!-- .wrapper -->
-        
-    <?php } ?>
+        </div><!-- .login-section -->
+    
+<?php } ?>
 
-</div><!-- .main -->
+    </div><!-- .index-wrapper -->
 
 
 <!-- FOOTER -->
