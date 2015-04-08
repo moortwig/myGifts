9cc9f6a2a973cd5a2fa26da0a84054ec4d96ca06
moortwig <info@moortwig.se>
2015-04-08 20:48:29 +0200

    Restructuring html

--- edititem.php
+++ edititem.php
@@ -3,7 +3,8 @@
 require_once('header.php');
 
 ?>
-<div class="main container">
+<div class="wrapper edit-item-page">
+	<div class="container">
     <?php
     // Display this section if a user is logged in
     // TODO EDIT FORM 
@@ -20,33 +21,43 @@ require_once('header.php');
 
         	// Check that data isn't null
         	if ($name && $description !== NULL) { ?>
-        		<div class="edit-item-form">
-		            <!--    TODO adjust this for the new structure!  -->
-		            <form class="form-horizontal" method="post" action="app.php" role="form">
-		                <label for="name">Name:</label>
-		                <input type="text" name="name" class="form-control" value="<?php echo $name; ?>" /><br />
-		                <label for="Description">Description:</label><br />
-		                <textarea name="description" class="form-control"><?php echo $description; ?></textarea>
-		                <!-- TODO Maybe a better way to pass on the session user ID? -->
-		                <!-- YES! THERE IS!!! Besides, the row below isn't even working ...
-		                Read THIS: http://www.clfsrpm.net/csrf/ -->
-		                <!-- <input name="userId" type="hidden" value=<?php /*echo "'". $_SESSION['userId'] . "'" */?> /> -->
-		                <!-- TODO remove this field with dummy data -->
-		                <input name="userId" type="hidden" value="5" />
-		                <input name="itemId" type="hidden" value="<?php echo $itemId; ?>" />
+	        	<div class="row">
+	            	<div class="col-md-4 col-md-offset-2">
+			            <!--    TODO adjust this for the new structure!  -->
+			            <form class="form-horizontal" method="post" action="app.php" role="form">
+			                <label for="name">Name:</label>
+			                <input type="text" name="name" class="form-control" value="<?php echo $name; ?>" /><br />
+			                <label for="Description">Description:</label><br />
+			                <textarea name="description" class="form-control"><?php echo $description; ?></textarea>
+			                <!-- TODO Maybe a better way to pass on the session user ID? -->
+			                <!-- YES! THERE IS!!! Besides, the row below isn't even working ...
+			                Read THIS: http://www.clfsrpm.net/csrf/ -->
+			                <!-- <input name="userId" type="hidden" value=<?php /*echo "'". $_SESSION['userId'] . "'" */?> /> -->
+			                <!-- TODO remove this field with dummy data -->
+			                <input name="userId" type="hidden" value="5" />
+			                <input name="itemId" type="hidden" value="<?php echo $itemId; ?>" />
 
-		                <br />
-		                <!-- buttons: -->
-		                <!-- TODO jQuery on click clear form
-		                <input type="submit" class="button" name="clear" value="CLEAR" /> -->
+			                <br />
+			                <!-- buttons: -->
+			                <!-- TODO jQuery on click clear form
+			                <input type="submit" class="button" name="clear" value="CLEAR" /> -->
 
-		                <!-- TODO JavaScript check that all fields are filled in: -->
-		                <!-- TODO jQuery on key down "Enter" -> Submit -->
-		                <button type="submit" class="btn btn-danger" name="deleteItem">Delete</button>
-		            <a href="profile.php" class="btn btn-warning">Cancel</a>
-		                <button type="submit" class="btn btn-success" name="editItem">Save</button>
-		            </form>
-	        	</div><!-- .edit-item-form -->
+			                <!-- TODO JavaScript check that all fields are filled in: -->
+			                <!-- TODO jQuery on key down "Enter" -> Submit -->
+			                <button type="submit" class="btn btn-danger" name="deleteItem">Delete</button>
+			            	<a href="profile.php" class="btn btn-warning">Cancel</a>
+			                <button type="submit" class="btn btn-success" name="editItem">Save</button>
+			            </form>
+
+	            		<br />
+	        		</div><!-- .col-md-4 col-md-offset-4 -->
+
+	        		<div class="col-md-6">
+	        			<p class="bg-info">
+	                    	Edit items here. Click "Cancel" if you change your mind, and want to go back to your profile!
+	                	</p>
+	        		</div><!-- .col-md-6 -->
+		        </div><!-- row -->
         	<?php } else {	     
         		// if data is null, redirect to an error page   	
 	        	header('location: 404.php');
@@ -58,6 +69,7 @@ require_once('header.php');
         <h2>Hold!</h2>
         <p>Access to this content is forbidden. Log in, or sign up and you shall recieve access.</p>
     <?php } ?>
+    </div><!-- .container -->
 </div> <!-- .main -->
 
 <!-- FOOTER -->
