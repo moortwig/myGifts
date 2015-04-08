e8492163cbf2961896b690854c73dfc6ca17f1e9
moortwig <info@moortwig.se>
2015-04-08 20:01:30 +0200

    moved signup form

--- index.php
+++ index.php
@@ -17,21 +17,42 @@ require_once('header.php');
         <div class="container">
             <h1 class="landing-headline">Lorem ipsum dolor sit amet, id mei impetus omnesque.</h1>
         </div><!-- .container -->
-    </div><!-- .landing -->
+        <div class="container">
+            <p class="landing-text">
+                Keep track of items you've given away.
+                Add your friends and family to your own registry.
+                Your gift reminder right there in your pocket.
+            </p>
+            <h4 class="form-signin-heading">Not registered yet?</h4>
+            <a href="signup.php" class="btn btn-lg btn-success">SIGN UP NOW</a>
+        </div><!-- .container -->
+    </div><!-- .landing-section -->
 
     <div class="sign-up-section">
-        <form class="form-horizontal" method="post" action="app.php" role="form">
-            <div class="container centered-content block-align">
-                
-                <div class="col-xs-12 col-md-3 align-columns">
-                    <h1>myGifts</h1>
-                    <p>Keep track of items you've given away.
-                    Add your friends and family to your own registry.
-                    Your gift reminder right there in your pocket.</p>
-                    <h4 class="form-signin-heading">Not registered yet?</h4><a href="signup.php" class="btn btn-lg btn-success btn-block">SIGN UP NOW</a>
-                </div>
-            </div><!-- .container -->
-        </form>
+        <div class="container">
+            <form class="form-signin" method="post" action="app.php" role="form">
+                <h2 class="form-signin-heading">Sign up</h2>
+
+                <label for="inputUsername" class="sr-only">>Username:</label>
+                <input type="text" name="username" class="form-control" placeholder="Username" />
+
+                <label for="inputPassword" class="sr-only">Password:</label>
+                <input type="password" name="password" class="form-control" placeholder="Password" />
+                <!-- TODO JavaScript regex password check below: -->
+                <!-- <label for="Passwordcheck">Retype password:</label>
+                <input type="password" name="pwdCheck" name="" class="form-control" placeholder="Retype password" /><br />-->
+                <label for="inputEmail" class="sr-only">E-mail:</label>
+                <input type="email" name="email" class="form-control" placeholder="E-mail" /><br />
+                <!-- buttons: -->
+                <!-- TODO jQuery on click clear form
+                <input type="submit" class="button" name="clear" value="CLEAR" /> -->
+
+                <!-- TODO JavaScript check that all fields are filled in: -->
+                <!-- TODO jQuery on key down "Enter" -> Submit -->
+                <button type="submit" class="btn btn-lg btn-primary btn-block" name="signup">SUBMIT</button>
+                <h4 class="form-signin-heading"><a href="index.php">Back to start</a></h4>
+            </form>
+        </div><!-- .container -->
     </div>
     
 <?php } ?>
