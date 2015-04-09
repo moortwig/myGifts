cd9c05ea0f24ffdb4bb7ce122be630a76dbb9742
moortwig <info@moortwig.se>
2015-04-09 23:34:22 +0200

    restructuring js

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,13 @@
+var myGifts = {
+	init: function() {
+		// functions here
+	}
+}
+
+
 $(document).ready(function() {
 	console.log("main js");
 	$(".nano").nanoScroller();
+	var height = $(window).innerHeight();
+	console.log(height);
 });
