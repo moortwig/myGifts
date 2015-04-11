4d4d723eb66824aca975a8d63befd132d9c301b2
moortwig <info@moortwig.se>
2015-04-11 20:42:46 +0200

    started items page

---
+++ items.php
@@ -0,0 +1,48 @@
+<?php 
+
+require_once('header.php');
+
+?>
+<div class="wrapper recipients">
+    <div class="container">
+        <?php
+        // Display this section if a user is logged in
+        if(isset($_SESSION['username'])) { ?>
+                       
+            <h2 class="">Items<span class="label"><a href="addrecipient.php"><i class="ton-li-plus circle"></i></a></span></h2>
+            <p class="help-block">All your items are displayed in this list.</p>
+            <?php
+            $recipient = new Recipient();
+            $userId = 5; // dummy data TODO change this to the session user
+            $recipients = $recipient->getAllRecipients($userId);
+           
+            foreach ($recipients as $key => $r) {
+                $recipientId = $r['id'];
+                                
+                echo "<div class='data-row'>"; 
+                    echo "<div class='data-field'>";
+                        echo "<a href='recipient.php?id=" . $r['id'] . "'>" . $r['name'] . "</a> ";
+                        echo "<a href='editrecipient.php?id=" . $r['id'] . "'><span class='glyphicon glyphicon-pencil'></span></a> ";
+                        // echo $r['name'];
+                    echo "</div>";
+                    echo "<div class='data-field'>";
+                        echo $r['information'];
+                    echo "</div>";
+                    // echo "<div class='data-edit'>";
+                        // echo "<a class='btn btn-default btn-xs' href='editrecipient.php?id=" . $r['id'] . "'>Edit</a> ";
+                    // echo "</div>";
+                echo "</div>"; ?>
+            <?php } ?>
+        <?php } else { ?>
+            <h2>Hold!</h2>
+            <p>Access to this content is forbidden. Log in, or sign up and you shall recieve access.</p>
+        <?php } ?>
+    </div><!-- .container -->
+</div> <!-- .wrapper -->
+
+<!-- FOOTER -->
+<?php 
+
+require_once('footer.php');
+
+?>
