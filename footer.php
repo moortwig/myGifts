b00b9811ad1d9191bfc15da088a34c2d22b382df
moortwig <info@moortwig.se>
2015-04-12 21:10:12 +0200

    working on fixing footer

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<div id="footer">
+<div id="footer" class="footer">
 	<!-- <div class="footer"> -->
 		<!-- <div class="container"> -->
 			<ul>
--- resources/js/main.js
+++ resources/js/main.js
@@ -7,7 +7,8 @@ var app = {
 		console.log('init');
 		// functions here
 		app.nano();
-		app.divHeight();		
+		app.divHeight();
+		app.stickyFooter();		
 	},
 
 
@@ -25,9 +26,20 @@ var app = {
 	*/
 	divHeight: function() {
 		console.log('sectionHeight');
-		var height = $(window).height();
-		$('.section-wrapper').css('height', height);
+		var landingHeight = $('.landing-section').height();
+		var windowHeight = $(window).height();
+		// var signupHeight = windowHeight - footerHeight;
+		// var footerHeight = $('#footer').height();
+		// console.log(signupHeight);
+
+		$('.section-wrapper').css('height', windowHeight);
+		// $('.section-wrapper').css('signupHeight', windowHeight - footerHeight);
 		// $('.landing-text').css('height', height*0.8);
+	},
+
+	stickyFooter: function() {
+		console.log('stickyFooter');
+		//
 	}
 }
 
