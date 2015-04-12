b1baba51781bcd79394d602a790e8c2a57650b1e
moortwig <info@moortwig.se>
2015-04-12 18:48:36 +0200

    github link

--- about.php
+++ about.php
@@ -10,7 +10,7 @@ require_once('header.php');
 		<p>
 			PBR&B mixtape squid wolf. Paleo asymmetrical single-origin coffee readymade, mustache four loko irony hoodie. Mixtape umami McSweeney's Vice post-ironic beard. Flannel cronut Schlitz distillery post-ironic YOLO, yr wolf. PBR&B trust fund chia, keytar Etsy literally lomo post-ironic. Church-key pork belly Portland, chia Marfa butcher meggings farm-to-table kitsch fashion axe pop-up Godard American Apparel. Helvetica cornhole freegan gluten-free, art party iPhone hoodie YOLO synth lo-fi Intelligentsia raw denim four dollar toast narwhal paleo.
 		</p>
-		<p>View this project on GitHub, (link here)</p>
+		<p>View this project on GitHub, <a href="https://github.com/moortwig/myGifts">https://github.com/moortwig/myGifts</a></p>
 	</div><!-- .container -->
 
 </div><!-- .wrapper -->
