66d8dd2b9bdb1a813785e4015c36e7131883a011
moortwig <info@moortwig.se>
2015-05-01 17:17:12 +0200

    changed div classes

--- addgift.php
+++ addgift.php
@@ -3,74 +3,76 @@
 require_once('header.php');
 
 ?>
-<div class="wrapper add-gift-page">
-    <div class="container">
-        <?php
-        // Display this section if a user is logged in
-        if(isset($_SESSION['username'])) {
-            $userId = $_SESSION['userId'];
-    		// $userId = 5; // TODO change this to a safe get user id
-            // TODO Sessions for item and recipient
-    		$item = new Item();
-    		// $items = $item->getAllItems($userId);
+<div class="add-gift-page">
+    <div class="add-gift-section">
+        <div class="container">
+            <?php
+            // Display this section if a user is logged in
+            if(isset($_SESSION['username'])) {
+                $userId = $_SESSION['userId'];
+        		// $userId = 5; // TODO change this to a safe get user id
+                // TODO Sessions for item and recipient
+        		$item = new Item();
+        		// $items = $item->getAllItems($userId);
 
-    		$recipient = new Recipient();
-    		// $recipients = $recipient->getAllRecipients($userId);
-    		?>
-            <div class="row">
-                <div class="col-xs-offset-2 col-md-offset-3 col-xs-8 col-md-4 col-xs-offset-2 col-md-offset-5">
-                    <div>
-                        <em>
-                            You added: 
-                            <?php
-                            $recipients = $_GET['recipients'];
-                            $recipientArray = json_decode($_GET['recipients']);
-                            $itemName = $_GET['itemName'];
-                            $description = $_GET['description'];
-
-
-                            echo $itemName . ' to ';
-                            foreach ($recipientArray as $key => $recipientId) {
-                                $recipientName = $recipient->getRecipient($recipientId)['name'];
-
-                                echo $recipientName;
-                                echo ', ';
-                            }
-                            ?>
-                        </em>
-                    </div>
-
-                    <h2>3: Name the occasion</h2>
-                    <form class="form-horizontal" method="post" action="app.php" role="form">
-                        
-                        <label for="Occasion" class="sr-only">Occasion:</label>
-                        <input type="text" name="occasion" class="form-control" placeholder="Occasion" required /><br />
-                        <!-- buttons: -->
-                        <!-- TODO JavaScript check that all fields are filled in: -->
-                        <!-- TODO jQuery on key down "Enter" -> Submit -->
-
-                        <!-- TODO Maybe a better way to pass on the session user ID? -->
-                        <!-- YES! THERE IS!!! Besides, the row below isn't even working ...
-                        Read THIS: http://www.clfsrpm.net/csrf/ -->
-                        <input name="userId" type="hidden" value=<?php echo "'". $_SESSION['userId'] . "'" ?> />
-                        <!-- TODO remove this field with dummy data -->
-                        <!-- <input name="userId" type="hidden" value="5" required /> -->
-                        <input name="itemName" type="hidden" value="<?php echo $itemName ?>" required />
-                        <input name="description" type="hidden" value="<?php echo $description ?>" required />
-                        <input name="recipients" type="hidden" value="<?php echo htmlspecialchars($recipients) ?>" required />
+        		$recipient = new Recipient();
+        		// $recipients = $recipient->getAllRecipients($userId);
+        		?>
+                <div class="row">
+                    <div class="col-xs-offset-2 col-md-offset-3 col-xs-8 col-md-4 col-xs-offset-2 col-md-offset-5">
+                        <div>
+                            <em>
+                                You added: 
+                                <?php
+                                $recipients = $_GET['recipients'];
+                                $recipientArray = json_decode($_GET['recipients']);
+                                $itemName = $_GET['itemName'];
+                                $description = $_GET['description'];
 
 
-                        <button type="submit" class="btn btn-lg btn-success" name="addGift">I'm all done!</button>
+                                echo $itemName . ' to ';
+                                foreach ($recipientArray as $key => $recipientId) {
+                                    $recipientName = $recipient->getRecipient($recipientId)['name'];
 
-                    </form>
-                </div><!-- .col* -->
-            </div><!-- .row -->
-        <?php } else { ?>
-            <h2>Hold!</h2>
-            <p>Access to this content is forbidden. Log in, or sign up and you shall recieve access.</p>
-        <?php } ?>
-    </div><!-- .container -->
-</div><!-- .wrapper  -->
+                                    echo $recipientName;
+                                    echo ', ';
+                                }
+                                ?>
+                            </em>
+                        </div>
+
+                        <h2>3: Name the occasion</h2>
+                        <form class="form-horizontal" method="post" action="app.php" role="form">
+                            
+                            <label for="Occasion" class="sr-only">Occasion:</label>
+                            <input type="text" name="occasion" class="form-control" placeholder="Occasion" required /><br />
+                            <!-- buttons: -->
+                            <!-- TODO JavaScript check that all fields are filled in: -->
+                            <!-- TODO jQuery on key down "Enter" -> Submit -->
+
+                            <!-- TODO Maybe a better way to pass on the session user ID? -->
+                            <!-- YES! THERE IS!!! Besides, the row below isn't even working ...
+                            Read THIS: http://www.clfsrpm.net/csrf/ -->
+                            <input name="userId" type="hidden" value=<?php echo "'". $_SESSION['userId'] . "'" ?> />
+                            <!-- TODO remove this field with dummy data -->
+                            <!-- <input name="userId" type="hidden" value="5" required /> -->
+                            <input name="itemName" type="hidden" value="<?php echo $itemName ?>" required />
+                            <input name="description" type="hidden" value="<?php echo $description ?>" required />
+                            <input name="recipients" type="hidden" value="<?php echo htmlspecialchars($recipients) ?>" required />
+
+
+                            <button type="submit" class="btn btn-lg btn-success" name="addGift">I'm all done!</button>
+
+                        </form>
+                    </div><!-- .col* -->
+                </div><!-- .row -->
+            <?php } else { ?>
+                <h2>Hold!</h2>
+                <p>Access to this content is forbidden. Log in, or sign up and you shall recieve access.</p>
+            <?php } ?>
+        </div><!-- .container -->
+    </div><!-- .add-gift-section -->
+</div><!-- .add-gift-page -->
 
 
 <!-- FOOTER -->
