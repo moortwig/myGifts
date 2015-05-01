49b9f08d104c4e2dbc03f2e466c1f790743128e9
moortwig <info@moortwig.se>
2015-05-01 17:18:30 +0200

    cleanup

--- additem.php
+++ additem.php
@@ -9,36 +9,32 @@ require_once('header.php');
             <?php
             // Display this section if a user is logged in
             if(isset($_SESSION['username'])) { ?>
-                <!-- <div class="row"> -->
-                    <div class="add-item-form">
-                    <!-- <div class="col-sm-4 col-sm-offset-2"> -->
-                    <!-- <div class="col-xs-offset-2 col-md-offset-3 col-xs-8 col-md-4 col-xs-offset-2 col-md-offset-5"> -->
-                        <form class="form-horizontal" method="post" action="app.php" role="form">
-                            <h2>Step 1: Add item</h2>
+                <div class="add-item-form">
+                    <form class="form-horizontal" method="post" action="app.php" role="form">
+                        <h2>Step 1: Add item</h2>
 
-                            <label for="itemName" class="sr-only">Name:</label>
-                            <input type="text" name="itemName" class="form-control" placeholder="Name" required/><br />
-                            <label for="description" class="sr-only">Description:</label><br />
-                            <textarea name="description" class="form-control" placeholder="Description"></textarea>
-                            <!-- TODO Maybe a better way to pass on the session user ID? -->
-                            <!-- YES! THERE IS!!! Besides, the row below isn't even working ...
-                            Read THIS: http://www.clfsrpm.net/csrf/ -->
-                          
-                            <input name="userId" type="hidden" value=<?php echo "'". $_SESSION['userId'] . "'" ?> />
-                            <!-- TODO remove this field with dummy data -->
-                            <!-- <input name="userId" type="hidden" value="5" required /> -->
-                            <br />
-                            <!-- buttons: -->
-                            <!-- TODO jQuery on click clear form
-                            <input type="submit" class="button" name="clear" value="CLEAR" /> -->
+                        <label for="itemName" class="sr-only">Name:</label>
+                        <input type="text" name="itemName" class="form-control" placeholder="Name" required/><br />
+                        <label for="description" class="sr-only">Description:</label><br />
+                        <textarea name="description" class="form-control" placeholder="Description"></textarea>
+                        <!-- TODO Maybe a better way to pass on the session user ID? -->
+                        <!-- YES! THERE IS!!! Besides, the row below isn't even working ...
+                        Read THIS: http://www.clfsrpm.net/csrf/ -->
+                      
+                        <input name="userId" type="hidden" value=<?php echo "'". $_SESSION['userId'] . "'" ?> />
+                        <!-- TODO remove this field with dummy data -->
+                        <!-- <input name="userId" type="hidden" value="5" required /> -->
+                        <br />
+                        <!-- buttons: -->
+                        <!-- TODO jQuery on click clear form
+                        <input type="submit" class="button" name="clear" value="CLEAR" /> -->
 
-                            <!-- TODO JavaScript check that all fields are filled in: -->
-                            <!-- TODO jQuery on key down "Enter" -> Submit -->
-                            <?php /*echo "<a href='chooserecipient.php?item=" . $recipientId . "' class='btn btn-xs btn-success'>Edit</a> ";*/ ?>
-                            <button type="submit" class="btn btn-md btn-success" name="addItemContinue"><span class="glyphicon glyphicon-share-alt"></span>Continue</button>
-                        </form>
-                    </div><!-- .add-item-form -->
-                <!-- </div>.row -->
+                        <!-- TODO JavaScript check that all fields are filled in: -->
+                        <!-- TODO jQuery on key down "Enter" -> Submit -->
+                        <?php /*echo "<a href='chooserecipient.php?item=" . $recipientId . "' class='btn btn-xs btn-success'>Edit</a> ";*/ ?>
+                        <button type="submit" class="btn btn-md btn-success" name="addItemContinue"><span class="glyphicon glyphicon-share-alt"></span>Continue</button>
+                    </form>
+                </div><!-- .add-item-form -->
             <?php } else { ?>
                 <h2>Hold!</h2>
                 <p>Access to this content is forbidden. Log in, or sign up and you shall recieve access.</p>
