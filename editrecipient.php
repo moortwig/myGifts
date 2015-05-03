7dae78dc24d5ac7cce804719c95167b4b93276e6
moortwig <info@moortwig.se>
2015-05-03 18:54:31 +0200

    fixes

--- editrecipient.php
+++ editrecipient.php
@@ -20,42 +20,32 @@ require_once('header.php');
 
 				// Check that data isn't null
 				if ($name && $information !== NULL) { ?>
-	        		<div class="row">
-	            		<div class="col-md-4 col-md-offset-2">
-							<h2>Edit Recipient</h2> 
-					        <!--    TODO adjust this for the new structure!  -->
-					        <form class="form-horizontal edit-recipient-form" method="post" action="app.php" role="form">
-					            <label for="name">Name:</label>
-					            <input type="text" name="name" class="form-control" value="<?php echo $name; ?>" /><br />
-					            <label for="Information">Information:</label><br />
-					            <textarea name="information" class="form-control"><?php echo $information; ?></textarea>
-					            <!-- TODO Maybe a better way to pass on the session user ID? -->
-					            <!-- YES! THERE IS!!! Besides, the row below isn't even working ...
-					            Read THIS: http://www.clfsrpm.net/csrf/ -->
-					            <!-- <input name="userId" type="hidden" value=<?php /*echo "'". $_SESSION['userId'] . "'" */?> /> -->
-					            <!-- TODO remove this field with dummy data -->
-					            <input name="userId" type="hidden" value="5" />
-					            <input name="recipientId" type="hidden" value="<?php echo $recipientId; ?>" />
+			        <!--    TODO adjust this for the new structure!  -->
+			        <form class="form-horizontal edit-recipient-form" method="post" action="app.php" role="form">
+						<h2>Edit Recipient</h2> 
+			            <label for="name">Name:</label>
+			            <input type="text" name="name" class="form-control" value="<?php echo $name; ?>" /><br />
+			            <label for="Information">Information:</label><br />
+			            <textarea name="information" class="form-control"><?php echo $information; ?></textarea>
+			            <!-- TODO Maybe a better way to pass on the session user ID? -->
+			            <!-- YES! THERE IS!!! Besides, the row below isn't even working ...
+			            Read THIS: http://www.clfsrpm.net/csrf/ -->
+			            <!-- <input name="userId" type="hidden" value=<?php /*echo "'". $_SESSION['userId'] . "'" */?> /> -->
+			            <!-- TODO remove this field with dummy data -->
+			            <input name="userId" type="hidden" value="5" />
+			            <input name="recipientId" type="hidden" value="<?php echo $recipientId; ?>" />
 
-					            <br />
-					            <!-- buttons: -->
-					            <!-- TODO jQuery on click clear form
-					            <input type="submit" class="button" name="clear" value="CLEAR" /> -->
+			            <br />
+			            <!-- buttons: -->
+			            <!-- TODO jQuery on click clear form
+			            <input type="submit" class="button" name="clear" value="CLEAR" /> -->
 
-					            <!-- TODO JavaScript check that all fields are filled in: -->
-					            <!-- TODO jQuery on key down "Enter" -> Submit -->
-					            <button type="submit" class="btn btn-danger" name="deleteRecipient">Delete</button>
-					        	<a href="recipients.php" class="btn btn-warning">Cancel</a>
-					            <button type="submit" class="btn btn-success" name="editRecipient">Save</button>
-					        </form>
-					    </div><!-- .col-md-4 col-md-offset-2 -->
-
-					   <!--  <div class="col-md-6">
-	        			<p class="bg-info">
-	                    	Edit a recipient here. Click "Cancel" if you change your mind, and want to go back to your profile!
-	                	</p>
-	        			</div> --><!-- .col-md-6 -->
-					</div><!-- .row -->
+			            <!-- TODO JavaScript check that all fields are filled in: -->
+			            <!-- TODO jQuery on key down "Enter" -> Submit -->
+			            <button type="submit" class="btn btn-danger" name="deleteRecipient">Delete</button>
+			        	<a href="recipients.php" class="btn btn-warning">Cancel</a>
+			            <button type="submit" class="btn btn-success" name="editRecipient">Save</button>
+			        </form>
 				<?php } else {	     
 					// if data is null, redirect to an error page   	
 					header('location: 404.php');
