d3342aa0864f91fc1027bfbf256a286306070282
moortwig <info@moortwig.se>
2015-05-03 22:12:07 +0200

    nanoscroll test. Ready for beta test 2

--- recipients.php
+++ recipients.php
@@ -4,37 +4,41 @@ require_once('header.php');
 
 ?>
 <div class="recipients-page">
-    <div class="recipients-section">
+    <div class="recipients-section"> 
+                <div id="recipients" class="nano">
+                    <div class="nano-content">
         <div class="container">
             <?php
             // Display this section if a user is logged in
             if(isset($_SESSION['username'])) { ?>
-                           
-                <h2 class="">Recipients<a href="addrecipient.php"><i class="ton-li-plus"></i></a></h2>
-                <p class="help-block">All your stored recipients are displayed in this list. Click on a name to see further details, such as gift history.</p>
-                <?php
-                $recipient = new Recipient();
-                $userId = $_SESSION['userId']; // dummy data TODO change this to the session user
-                $recipients = $recipient->getAllRecipients($userId);
                
-                foreach ($recipients as $key => $r) {
-                    $recipientId = $r['id'];
-                                    
-                    echo "<div class='data-row'>"; 
-                        echo "<div class='data-field'>";
-                            echo "<a href='recipient.php?id=" . $r['id'] . "'>" . "<h4>" . $r['name'] . "</a>";
-                            echo "<a href='editrecipient.php?id=" . $r['id'] . "'><span class='glyphicon glyphicon-pencil'></span></a> " . "</h4>";
-                        echo "</div>";
-                        echo "<div class='data-field'>";
-                            echo "<p>" . $r['information'] . "</p>";
-                        echo "</div>";
-                    echo "</div>"; ?>
-                <?php } ?>
+                        <h2 class="">Recipients<a href="addrecipient.php"><i class="ton-li-plus"></i></a></h2>
+                        <p class="help-block">All your stored recipients are displayed in this list. Click on a name to see further details, such as gift history.</p>
+                        <?php
+                        $recipient = new Recipient();
+                        $userId = $_SESSION['userId']; // dummy data TODO change this to the session user
+                        $recipients = $recipient->getAllRecipients($userId);
+                       
+                        foreach ($recipients as $key => $r) {
+                            $recipientId = $r['id'];
+                                            
+                            echo "<div class='data-row'>"; 
+                                echo "<div class='data-field'>";
+                                    echo "<a href='recipient.php?id=" . $r['id'] . "'>" . "<h4>" . $r['name'] . "</a>";
+                                    echo "<a href='editrecipient.php?id=" . $r['id'] . "'><span class='glyphicon glyphicon-pencil'></span></a> " . "</h4>";
+                                echo "</div>";
+                                echo "<div class='data-field'>";
+                                    echo "<p>" . $r['information'] . "</p>";
+                                echo "</div>";
+                            echo "</div>"; ?>
+                        <?php } ?>
             <?php } else { ?>
                 <h2>Hold!</h2>
                 <p>Access to this content is forbidden. Log in, or sign up and you shall recieve access.</p>
             <?php } ?>
         </div><!-- .container -->
+                    </div><!-- .nano-content -->
+                </div><!-- #add-gift-recipients nano -->
     </div><!-- .recipients-section -->
 </div> <!-- .recipients-page -->
 
