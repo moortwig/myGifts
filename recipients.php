b8d557da6536bd4a72f0c78bab95b4ea0440777c
moortwig <info@moortwig.se>
2015-05-05 20:48:43 +0200

    display number of gifts

--- recipients.php
+++ recipients.php
@@ -12,17 +12,25 @@ require_once('header.php');
                
                 <h2 class="">Recipients<a href="addrecipient.php"><i class="ton-li-plus"></i></a></h2>
                 <p class="help-block">All your stored recipients are displayed in this list. Click on a name to see further details, such as gift history.</p>
+                
                 <?php
+                // get recipients:
                 $recipient = new Recipient();
-                $userId = $_SESSION['userId']; // dummy data TODO change this to the session user
+                $userId = $_SESSION['userId'];
                 $recipients = $recipient->getAllRecipients($userId);
+
+                $gift = new Gift();
+
                
                 foreach ($recipients as $key => $r) {
                     $recipientId = $r['id'];
+                    $gifts = count($gift->getAllGifts($recipientId));
                                     
                     echo "<div class='data-row'>"; 
                         echo "<div class='data-field'>";
+                            // print_r(count($gifts));
                             echo "<a href='recipient.php?id=" . $r['id'] . "'>" . "<h4>" . $r['name'] . "</a>";
+                            echo "<small><em> - " . $gifts . " gifts</em></small>";
                             echo "<a href='editrecipient.php?id=" . $r['id'] . "'><span class='glyphicon glyphicon-pencil'></span></a> " . "</h4>";
                         echo "</div>";
                         echo "<div class='data-field'>";
