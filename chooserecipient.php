ac88e659c4ab7b344acc7b6543e5aa4f0533bf36
moortwig <info@moortwig.se>
2015-05-05 23:17:45 +0200

    documentation

--- chooserecipient.php
+++ chooserecipient.php
@@ -10,7 +10,6 @@ require_once('header.php');
             // Display this section if a user is logged in
             if(isset($_SESSION['username'])) {
                 $userId = $_SESSION['userId'];
-                // $userId = 5; // TODO change this to a safe get user id
 
                 $recipient = new Recipient();
                 $recipients = $recipient->getAllRecipients($userId);
@@ -30,36 +29,29 @@ require_once('header.php');
                     <h3>Step 2: Choose recipient(s)</h3>
                     <div id="add-gift-recipients" class="nano">
                         <div class="nano-content">
-                            <?php
+                            <?php if ($recipients == NULL) {
+                                echo "<span class='error'>" . nl2br("You haven't added any recipients yet!\n Please click below to add them. \n" . "</span>");
+                                // echo "";
+                                echo "<a href='addrecipient.php' class='btn btn-info'>Add recipient</a>";
+                            } else { ?>
+                                <label for="Recipient">Recipient:</label>
+                                <?php foreach ($recipients as $recipient => $r) { ?>
+                                    <ul class="list-unstyled">
+                                    <li><input type="checkbox" name="recipientId[]" value="<?php echo htmlspecialchars($r['id']); ?>" />
+                                        <?php echo $r['name']; ?>
+                                    </input></li>
+                                    </ul>
+                                <?php } ?> <!-- ends foreach -->
 
-                    if ($recipients == NULL) {
-                        echo "<span class='error'>" . nl2br("You haven't added any recipients yet!\n Please click below to add them. \n" . "</span>");
-                        // echo "";
-                        echo "<a href='addrecipient.php' class='btn btn-info'>Add recipient</a>";
-                    } else {
-                    ?>
-                            <label for="Recipient">Recipient:</label>
-                            <?php
-                            foreach ($recipients as $recipient => $r) { ?>
-                                <ul class="list-unstyled">
-                                <li><input type="checkbox" name="recipientId[]" value="<?php echo htmlspecialchars($r['id']); ?>" />
-                                    <?php echo $r['name']; ?>
-                                </input></li>
-                                </ul>
-                            <?php } ?>
+                                <input name="itemName" type="hidden" value="<?php echo $itemName; ?>" required />
+                                <input name="description" type="hidden" value="<?php echo $description; ?>" required />
+                            </div><!-- .nano-content -->
+                        </div><!-- #add-gift-recipients nano -->
 
-                            <!-- <p class="help-block">Missing someone?</p>
-                            <a class="btn btn-sm btn-primary" href="addrecipient.php">Add someone!</a> -->
-                            <input name="itemName" type="hidden" value="<?php echo $itemName; ?>" required />
-                            <input name="description" type="hidden" value="<?php echo $description; ?>" required />
-                        </div><!-- .nano-content -->
-                    </div><!-- #add-gift-recipients nano -->
-
-                    <button type="submit" class="btn btn-md btn-success" name="chooseRecipient"><span class="glyphicon glyphicon-share-alt"></span>Continue</button>
-                    <?php } ?>
-                    <!-- <button type="submit" class="btn btn-md btn-success" name="storeRecipient"><span class="glyphicon glyphicon-share-alt"></span>Continue</button> -->
+                        <button type="submit" class="btn btn-md btn-success" name="chooseRecipient"><span class="glyphicon glyphicon-share-alt"></span>Continue</button>
+                    <?php } ?> <!-- ends if statement (recipients) -->
                 </form>
-            <?php } else { ?>
+            <?php } else { ?><!-- ends if statement (session) -->
                 <h2>Hold!</h2>
                 <p>Access to this content is forbidden. Log in, or sign up and you shall recieve access.</p>
             <?php } ?>
